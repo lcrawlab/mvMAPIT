87222d0148e0f1cd4cd87a98e33142d49ff1ca8b
Julian Stamp <julian.d.stamp@gmail.com>
2021-04-09 15:53:03 -0400

    MM-9: update documentation

--- data-raw/simulate_multiple_phenotypes.R
+++ data-raw/simulate_multiple_phenotypes.R
@@ -1,13 +1,13 @@
 set.seed(982348)
-library(dplyr)
 library('MASS')
 
-load('data/random_genotype_matrix.rda')
+file_name <- 'data/random_genotype_matrix.rda'
+load(file_name)
 
 X <- random_genotype_matrix
-X_mean <- apply(X, 2, mean)
-X_sd <- apply(X, 2, sd)
-X <- ((t(X) - X_mean) / X_sd)
+X_mean <- apply(X, 1, mean)
+X_sd <- apply(X, 1, sd)
+X <- ((X - X_mean) / X_sd)
 
 n_samples <- nrow(X) # number of genotype samples
 n_snp <- ncol(X) # number of SNPs
@@ -20,6 +20,7 @@ epistatic_correlation <- 0.3
 n_causal <- 20 # number of SNPs to be causal in every phenotype
 n_causal_epi <- 3 # number of epistatic causal SNPs slected per group and phenotype
 n_causal_pleio_epi <- 5 # number of SNPs to be involved in epistatic interactions in every phenotype
+snp.ids <- 1:n_snp
 
 Y_marginal <- c()
 Y_epistatic <- c()
@@ -28,14 +29,13 @@ causal_snps <- list()
 pleiotropic_set <- sample(snp.ids, n_causal_pleio_epi, replace = F)
 
 for (j in 1:d) {
-  ## Select Causal SNPs
-
+  ## select causal SNPs
   causal_snps_j <- sample(snp.ids[-pleiotropic_set], n_causal, replace = F)
   epistatic_set_j_1 <- sample(causal_snps_j, n_causal_epi, replace = F)
   epistatic_set_j_2 <- sample(causal_snps_j[! causal_snps_j %in% epistatic_set_j_1], n_causal_epi, replace = F)
   epistatic_set_j_1 <- c(epistatic_set_j_1, pleiotropic_set)
 
-  # Create Causal Epistatic Matrix
+  # create epistatic interaction matrix
   X_causal_j <- X[, causal_snps_j]
   X_epistatic_j_1 <- X[, epistatic_set_j_1]
   X_epistatic_j_2 <- X[, epistatic_set_j_2]
@@ -45,20 +45,20 @@ for (j in 1:d) {
   }
   dim(X_epi)
 
-  # Marginal Effects Only
+  # marginal effects
   X_marginal <- X_causal_j
   beta <- rnorm(dim(X_marginal)[2])
   y_marginal <- X_marginal %*% beta
   beta = beta * sqrt(H2 * rho / c(var(y_marginal)))
   y_marginal=X_marginal %*% beta
 
-  # Pairwise Epistatic Effects
+  # pairwise epistatic effects
   alpha <- rnorm(dim(X_epi)[2])
   y_epi <- X_epi %*% alpha
   alpha = alpha * sqrt(H2 * (1 - rho) / c(var(y_epi)))
   y_epi = X_epi %*% alpha
 
-  # Unexplained phenotypic variation
+  # unexplained phenotypic variation
   y_err <- rnorm(n_samples)
   y_err <- y_err * sqrt((1 - H2) / c(var(y_err)))
 
@@ -78,24 +78,31 @@ for (j in 1:d) {
 C <- matrix(marginal_correlation, ncol = d, nrow = d)
 diag(C) <- 1
 Y_marginal <- Y_marginal %*% chol(C)
-Y_marginal <- Y_marginal %*% (sqrt(H2 * rho) * ginv((diag(diag(var(Y_marginal))))^(1/2)))
 
 # scale epistatic data variance and correlation
 C <- matrix(epistatic_correlation, ncol = d, nrow = d)
 diag(C) <- 1
 Y_epistatic <- Y_epistatic %*% chol(C)
-Y_epistatic <- Y_epistatic %*% (sqrt( H2 * (1 - rho)) * ginv((diag(diag(var(Y_epistatic))))^(1/2)))
 
 # scale error variance
 C <- matrix(0, ncol = d, nrow = d)
 diag(C) <- 1
 Y_error <- Y_error %*% chol(C)
-Y_error <- Y_error %*% (sqrt( (1 - H2)) * ginv((diag(diag(var(Y_error))))^(1/2)))
 
 Y <- Y_marginal + Y_epistatic + Y_error
 
-# check dimensions and add SNP names
-dim(X)
-dim(Y)
 colnames(X) <- seq_len(ncol(X)) %>% sprintf(fmt = "snp_%05d") # column names names for SNPs
-colnames(Y) <- seq_len(ncol(Y)) %>% sprintf(fmt = "p_%02d") # column names names for SNPs
+colnames(Y) <- seq_len(ncol(Y)) %>% sprintf(fmt = "p_%02d") # column names names for phenotypes
+
+# export data
+simulated_pleiotropic_epistasis_data <- list(
+  number_snp = n_snp,
+  number_samples = n_samples,
+  pve = H2,
+  rho = rho,
+  phenotype = Y,
+  genotype = file_name,
+  causal_snps = causal_snps
+)
+
+usethis::use_data(simulated_pleiotropic_epistasis_data, overwrite = TRUE)
