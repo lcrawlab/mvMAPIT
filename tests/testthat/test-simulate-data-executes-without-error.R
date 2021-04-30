3baf66e37b0f27a689f4f87ffa9e93e0d72742bb
Julian Stamp <julian.d.stamp@gmail.com>
2021-04-30 13:56:45 -0400

    MM-18: use foreach for faster simulations

--- tests/testthat/test-simulate-data-executes-without-error.R
+++ tests/testthat/test-simulate-data-executes-without-error.R
@@ -1,4 +1,4 @@
-test_that("Simulate multiple phenotypes returns apropriate object", {
+test_that("Simulate multiple phenotypes returns apropriate phenotype object", {
   # given
   p <- 10
   n <- 5
@@ -11,5 +11,23 @@ test_that("Simulate multiple phenotypes returns apropriate object", {
   # then
   expect_equal(nrow(data$phenotype), n)
   expect_equal(ncol(data$phenotype), d)
+  expect_equal(sum(is.na(data$phenotype)), 0) # no NA values in phenotype
   expect_equal(length(data), 7)
+})
+
+test_that("Simulate multiple phenotypes returns causal SNPs", {
+  # given
+  p <- 10
+  f <- 0.4
+  g <- 0.1
+  n <- 5
+  d <- 3
+  X <- matrix(runif(p * n), ncol = p)
+  total_causal <- (f * p) + ceiling((f * p) * g) # single trait SNPs plus pleiotropic SNPs
+  
+  # when
+  data <- simulate_phenotypes(X, causal_fraction = f, pleiotropic_fraction = g, d = d, logLevel = 'DEBUG')
+  
+  # then
+  expect_equal(length(data$causal_snps$phenotype_1), total_causal)
 })
