fa39f85ac58c0c073dc9a03b20f574588a354972
Julian Stamp <julian.d.stamp@gmail.com>
2022-01-21 17:19:28 -0500

    MM-43: mvMAPIT combinatorial converges to MAPIT if phenotypes are identical

---
+++ tests/testthat/test-mvmapit-with-identical-phenotypes.R
@@ -0,0 +1,20 @@
+test_that("test that all pvalues equal if phenotypes equal", {
+  # given
+  p <- 20
+  n <- 10
+  set.seed(853)
+  X <- matrix(runif(p * n), ncol = p)
+  y <- runif(n)
+  Y <- as.matrix(cbind(y, y))
+  # when
+  mapit <- MvMAPIT(t(X),
+                   t(Y),
+                   test = 'normal',
+                   cores = 1,
+                   phenotypeCovariance = 'combinatorial',
+                   logLevel = "DEBUG")
+  # then
+  expect_equal(mapit$pvalues[, 1], mapit$pvalues[, 2], tolerance = 1e-8)
+  expect_equal(mapit$pvalues[, 1], mapit$pvalues[, 3], tolerance = 1e-8)
+})
+
