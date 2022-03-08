384da04d93eb8edc0f1eb2de55440bec05e31c71
Julian Stamp <julian.d.stamp@gmail.com>
2022-03-08 16:28:19 -0500

    MM-52: automate installation

--- oscar-tasks.py
+++ oscar-tasks.py
@@ -15,7 +15,19 @@ def _cmd(command, args=[]):
     return run(command.split(" ") + args)
 
 
+def _use_dev_files(tmpdir, files):
+    logger.info(f"Make .dev files available: {files}")
+    for f in files:
+        src = os.path.join(tmpdir, f)
+        dst = os.path.join(tmpdir, f.removesuffix(".dev"))
+        shutil.copyfile(src, dst)
+        logger.info(f"Copied {src} to {dst}")
+
+
 def task_rinstall(tmpdir):
     logger.info(f"Temporary directory {tmpdir}.")
+    dev_files = ["src/Makevars.in.dev", "src/MAPIT.h.dev"]
+    _use_dev_files(tmpdir, dev_files)
+
     logger.info(f"Install mvMAPIT.")
     _cmd(f"R CMD INSTALL {tmpdir} --preclean")
