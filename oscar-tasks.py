0027a0a7990fa13bc7f5fa46849a6f768b08acc1
jdstamp <julian.d.stamp@gmail.com>
2022-04-01 15:51:01 -0400

    Mm 50 tag last version with kronecker method (#51)
    * MM-50: automate versioning
    
    * MM-50: update version

--- oscar-tasks.py
+++ oscar-tasks.py
@@ -2,7 +2,10 @@
 import logging
 import os
 import shutil
+import re
+import fire
 from subprocess import run
+from sh import sed
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("mvmapit-oscar-tasks")
@@ -28,3 +31,20 @@ def task_rinstall(tmpdir):
 
     logger.info(f"Install mvMAPIT.")
     _cmd(f"R CMD INSTALL {tmpdir} --preclean")
+
+
+def change_version(version):
+    files = {
+        "DESCRIPTION": f"s/(Version: )[0-9.]*/\\1{version}/",
+        "configure.ac": f"s/[0-9]+\.[0-9]+\.[0-9]+/{version}/",
+    }
+
+    logger.info(f"Version: {version}")
+    for key, sedcmd in files.items():
+        sed(["-i", ".bu", "-E", sedcmd, key])
+
+    _cmd("autoconf")
+
+
+if __name__ == "__main__":
+    fire.Fire()
