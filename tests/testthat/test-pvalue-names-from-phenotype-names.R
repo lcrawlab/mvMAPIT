18d2ea0d2a59b46218b9df7116def4d898b42f84
Julian Stamp <julian.d.stamp@gmail.com>
2022-04-03 18:06:40 -0400

    MM-51: autoformatting

--- tests/testthat/test-pvalue-names-from-phenotype-names.R
+++ tests/testthat/test-pvalue-names-from-phenotype-names.R
@@ -1,30 +1,40 @@
-test_that("combinatorial test with d = 3. combinatorial.", {
-  # given
-  n <- 5
-  d <- 3
-  set.seed(853)
-  phenotypes <- sprintf("p%s", 1:d)
-  y <- matrix(runif(d * n), ncol = d)
-  colnames(y) <- phenotypes
-  correct_colnames <- c("p1*p1", "p2*p1", "p2*p2", "p3*p1", "p3*p2", "p3*p3")
-  # when
-  result <- mapit_struct_names(t(y))
-  # then
-  expect_equal(result, correct_colnames)
-})
+test_that(
+    "combinatorial test with d = 3. combinatorial.", {
+        # given
+        n <- 5
+        d <- 3
+        set.seed(853)
+        phenotypes <- sprintf("p%s", 1:d)
+        y <- matrix(
+            runif(d * n),
+            ncol = d
+        )
+        colnames(y) <- phenotypes
+        correct_colnames <- c("p1*p1", "p2*p1", "p2*p2", "p3*p1", "p3*p2", "p3*p3")
+        # when
+        result <- mapit_struct_names(t(y))
+        # then
+        expect_equal(result, correct_colnames)
+    }
+)
 
-test_that("combinatorial test with d = 1. combinatorial.", {
-  # given
-  n <- 5
-  d <- 1
-  set.seed(853)
-  phenotypes <- sprintf("p%s", 1:d)
-  y <- matrix(runif(d * n), ncol = d)
-  colnames(y) <- phenotypes
-  correct_colnames <- c("p1")
-  # when
-  result <- mapit_struct_names(t(y))
-  # then
-  expect_equal(result, correct_colnames)
-})
+test_that(
+    "combinatorial test with d = 1. combinatorial.", {
+        # given
+        n <- 5
+        d <- 1
+        set.seed(853)
+        phenotypes <- sprintf("p%s", 1:d)
+        y <- matrix(
+            runif(d * n),
+            ncol = d
+        )
+        colnames(y) <- phenotypes
+        correct_colnames <- c("p1")
+        # when
+        result <- mapit_struct_names(t(y))
+        # then
+        expect_equal(result, correct_colnames)
+    }
+)
 
--- tests/testthat/test-saddlepoint-approximation.R
+++ tests/testthat/test-saddlepoint-approximation.R
@@ -1,14 +1,16 @@
-test_that("saddlepoint approximation and davies similar results", {
-  # given
-  x <- c(1, 5, 11, 15, 20)
-  lambda <- rep(1, 10)
-  # when
-  davies_results <- c()
-  saddle_results <- c()
-  for (i in x) {
-    saddle_results <- c(saddle_results, saddlepoint_approximation(i, lambda))
-    davies_results <- c(davies_results, davies(i, lambda = lambda)$Qq)
-  }
-  # then
-  expect_equal(saddle_results, davies_results, tolerance = 1e-3)
-})
+test_that(
+    "saddlepoint approximation and davies similar results", {
+        # given
+        x <- c(1, 5, 11, 15, 20)
+        lambda <- rep(1, 10)
+        # when
+        davies_results <- c()
+        saddle_results <- c()
+        for (i in x) {
+            saddle_results <- c(saddle_results, saddlepoint_approximation(i, lambda))
+            davies_results <- c(davies_results, davies(i, lambda = lambda)$Qq)
+        }
+        # then
+        expect_equal(saddle_results, davies_results, tolerance = 0.001)
+    }
+)
