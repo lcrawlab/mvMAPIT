57b0b53b9b1ced01658fdf3271246f8abbaf7229
Julian Stamp <julian.d.stamp@gmail.com>
2022-10-26 15:03:43 -0400

    MM-82: starting point github pages

---
+++ docs/conf.py
@@ -0,0 +1,118 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import shutil
+import sys
+
+# -- Path setup --------------------------------------------------------------
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath(".."))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "mvMAPIT"
+copyright = "2022, lcrawlab"
+author = "lcrawlab"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx_immaterial"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_immaterial"  # "alabaster"
+html_static_path = ["_static"]
+
+# The name for this set of Sphinx documents.  If None, it defaults to
+html_static_path = ["_static"]
+# html_css_files = ["extra_css.css"]
+html_last_updated_fmt = ""
+html_title = "mvMAPIT"
+html_favicon = "_static/images/favicon.ico"
+html_logo = "_static/images/CrawfordLab.png"
+
+html_theme_options = {
+    "icon": {
+        "repo": "fontawesome/brands/github",
+    },
+    "site_url": "https://lcrawlab.github.io/susiepca/",
+    "repo_url": "https://github.com/lcrawlab/mvMAPIT/",
+    "repo_name": "mvMAPIT",
+    "repo_type": "github",
+    "edit_uri": "blob/main/docs",
+    "globaltoc_collapse": True,
+    "features": [
+        "navigation.expand",
+        # "navigation.tabs",
+        # "toc.integrate",
+        "navigation.sections",
+        # "navigation.instant",
+        # "header.autohide",
+        "navigation.top",
+        # "navigation.tracking",
+        # "search.highlight",
+        "search.share",
+        "toc.follow",
+        "toc.sticky",
+    ],
+    "palette": [
+        {
+            "media": "(prefers-color-scheme: light)",
+            "scheme": "default",
+            "primary": "teal",
+            "accent": "light-blue",
+            "toggle": {
+                "icon": "material/lightbulb-outline",
+                "name": "Switch to dark mode",
+            },
+        },
+        {
+            "media": "(prefers-color-scheme: dark)",
+            "scheme": "slate",
+            "primary": "teal",
+            "accent": "lime",
+            "toggle": {
+                "icon": "material/lightbulb",
+                "name": "Switch to light mode",
+            },
+        },
+    ],
+    # BEGIN: version_dropdown
+    "version_dropdown": True,
+    "version_info": [
+        {
+            "version": "https://lcrawlab.github.io/mvMAPIT",
+            "title": "Github Pages",
+            "aliases": [],
+        },
+    ],
+    # END: version_dropdown
+    "toc_title_is_page_title": True,
+    # BEGIN: social icons
+    "social": [
+        {
+            "icon": "fontawesome/brands/github",
+            "link": "https://github.com/lcrawlab/mvMAPIT",
+        },
+        # {
+        #    "icon": "fontawesome/brands/python",
+        #    "link": "https://pypi.org/project/sphinx-immaterial/",
+        # },
+    ],
+    # END: social icons
+}
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = "mvMAPIT-doc"
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. mvMAPIT documentation master file, created by
+   sphinx-quickstart on Wed Oct 26 13:48:37 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to mvMAPIT's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
