9d909ce3263b9c7c827905a400253e287402952b
Julian Stamp <julian.d.stamp@gmail.com>
2022-11-20 19:41:12 -0500

    MM-92: Phillips vignette

--- R/mvmapit_data.R
+++ R/mvmapit_data.R
@@ -4,11 +4,11 @@
 #' the fisher combined p-values, and the result from an exhaustive search using
 #' regression on the SNPs that were significant in the mvMAPIT analysis.
 #'
-#' @format ## `mvmapit_data`
+#' @format
 #' A nested list containing tibble data frames:
 #' \describe{
 #'   \item{mvmapit}{mvmapit return object; named list containing tibbles `pvalues`, `pves`, and `duration`.}
-#'   \item{fisher}{Tibble containing fisher combiend p-values of the mvmapit data.}
+#'   \item{fisher}{Tibble containing fisher combined p-values of the mvmapit data.}
 #'   \item{exhaustive_search}{A dataframe containing the p-values of an exhaustive search together with the analysed interaction pair.}
 #' }
 #' @source data-raw/mvmapit_on_simulated_data.R
